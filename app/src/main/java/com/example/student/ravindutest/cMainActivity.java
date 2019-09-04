9b01c0a2a56bf78a75b437f37b1fb8db923a3400
cjhcck96 <cjhcck96@gmail.com>
2019-09-04 16:48:34 +0530

    second Commite

---
+++ app/src/main/java/com/example/student/ravindutest/cMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.student.ravindutest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class cMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cactivity_main);
+    }
+}
